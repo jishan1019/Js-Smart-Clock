35a299968070f788869f167e346ea757cdd1f4b9
jishan1019 <imranislamjishan200@gmail.com>
2023-10-26 19:52:51 +0600

    current time lessthen 0 add before display

--- js/clock.js
+++ js/clock.js
@@ -5,7 +5,10 @@ let second = document.querySelector("#second");
 //set the current time
 setInterval(() => {
   let currentTime = new Date();
-  hours.innerHTML = currentTime.getHours();
-  minute.innerHTML = currentTime.getMinutes();
-  second.innerHTML = currentTime.getSeconds();
+  hours.innerHTML =
+    (currentTime.getHours() < 10 ? "0" : "") + currentTime.getHours();
+  minute.innerHTML =
+    (currentTime.getMinutes() < 10 ? "0" : "") + currentTime.getMinutes();
+  second.innerHTML =
+    (currentTime.getSeconds() < 10 ? "0" : "") + currentTime.getSeconds();
 });
